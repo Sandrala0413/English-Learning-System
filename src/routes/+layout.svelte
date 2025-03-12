3bc9821a028611d13e1287d5131553d1981cb1d1
tsaiyunshan <acs111129@gm.ntcu.edu.tw>
2025-03-12 22:16:59 +0800

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script>
 	import '../app.css';
-	let { children } = $props();
+	// let { children } = $props();
 </script>
 
-{@render children()}
+<!-- <header>
+	<ul>
+		<li><a href="/test">test</a></li>
+	</ul>
+</header> -->
+
+<style>
+  :global(body) {
+    background: linear-gradient(135deg, #c5d5fa, #c3dc99, #fad0c4);
+    height: 100vh;
+    margin: 0;
+  }
+</style>
+
+<slot />
+
+<!-- {@render children()} -->
