39b13d20d2646eda81738c7fe6ba01f635a3ec84
tsaiyunshan <acs111129@gm.ntcu.edu.tw>
2025-03-14 11:22:43 +0800

    commit

---
+++ src/lib/card/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {card, wordCss, speechCss, pronounceBox, pronounceCss, defineCss, sentenceCss, defineBox} from "./Card.style";
+
+    export let word = "單字";
+    export let speech = "詞性";
+    export let audioSrc = "";
+    export let pronounce = "kk音標";
+    export let define = "單字解釋";
+    export let sentence = "句子";
+
+    function playAudio() {
+        let audio = new Audio(audioSrc);
+        audio.play();
+    }
+</script>
+
+<div class={card}>
+    <p class={wordCss}>{word}</p>
+    <p class={speechCss}>{speech}</p>
+    <div class={pronounceBox}>
+        <i class="fa-solid fa-volume-high" style="color: #859fcc;" on:click={playAudio}></i>
+        <p class={pronounceCss}>{pronounce}</p>
+    </div>
+    <p class={defineBox}>解釋</p>
+    <p class={defineCss}>{define}</p>
+    <p class={defineBox}>例句</p>
+    <p class={sentenceCss}>{sentence}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<script>
+    import Card from "$lib/card/Card.svelte";
+</script>
+
+<Card 
+    word="Eloquent"
+    speech="adjective"
+    audioSrc ="https://dictionary.cambridge.org/zht/media/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/uk_pron/u/uke/ukeli/ukelimi026.mp3"
+    pronounce=" /ˈel.ə.kwənt/"
+    define="表達流暢的，說話或寫作很有說服力。"
+    sentence="His speech was eloquent and inspiring."
+/>
